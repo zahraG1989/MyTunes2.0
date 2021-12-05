1985d504c6d77f2d78c47a9e088517662f476d5a
Chantal1208 <chan2292@easv365.dk>
2021-12-05 09:38:00 +0100

    Button fuctions

--- src/GUI/Controllers/Controller.java
+++ src/GUI/Controllers/Controller.java
@@ -12,12 +12,24 @@ import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 public class Controller {
+    public Button btnDeleteSongFromPlaylist;
+    public Button btnNewSong;
+    public Button btnAddSong;
+    public Button btnEditSongs;
     @FXML
-    private Button btnNewSong;
+    private Button btnDeleteSong;
     @FXML
     private Button btnAppClose;
     @FXML
     private Button btnNewPlaylist;
+    @FXML
+    private Button btnDeletePlaylist; 
+    @FXML
+    private Button btnEditPlaylist;
+    @FXML
+    private Button btnMoveSongDown;
+    @FXML
+    private Button btnMoveSongUp;
     @FXML
     private Label welcomeText;
 
@@ -39,10 +51,50 @@ public class Controller {
 
     }
     public void openNewSongWindow(ActionEvent actionEvent) {
-        System.out.println("Läuft");
+        System.out.println("Works");
     }
 
     public void openNewPlaylistWindow(ActionEvent actionEvent) {
-        System.out.println("Läuft Auch");
+        System.out.println("works too");
+    }
+
+    public void deletePlaylist(ActionEvent actionEvent) {
+        System.out.println("works too");
+    }
+
+    public void editPlaylist(ActionEvent actionEvent) {
+        System.out.println("works too");
+    }
+
+    public void moveSongDown(ActionEvent actionEvent) {
+        System.out.println("works too");
+    }
+
+    public void moveSongUp(ActionEvent actionEvent) {
+        System.out.println("works too");
+    }
+
+    public void deleteSongFromPlaylist(ActionEvent actionEvent) {
+        System.out.println("works too");
+    }
+
+    public void addSongToPlaylist(ActionEvent actionEvent) {
+        System.out.println("works too");
+    }
+
+    public void editSong(ActionEvent actionEvent) {
+        System.out.println("works too");
+    }
+
+    public void deleteSong(ActionEvent actionEvent) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "Delete the Song ?", ButtonType.YES, ButtonType.NO, ButtonType.CANCEL);
+        alert.showAndWait();
+        //comments
+
+        if (alert.getResult() == ButtonType.YES) {
+
+            Stage stage = (Stage) btnDeleteSong.getScene().getWindow();
+            stage.close();
+        }
     }
 }
