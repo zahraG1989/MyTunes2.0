a1e953fabd530427b9e3459214546e175f7e7da4
zahraG1989 <93261667+zahraG1989@users.noreply.github.com>
2021-12-06 11:47:18 +0100

    db

--- src/GUI/Controllers/Controller.java
+++ src/GUI/Controllers/Controller.java
@@ -4,18 +4,20 @@ import javafx.embed.swing.JFXPanel;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
-import javafx.scene.control.ButtonType;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
+
+import java.io.File;
 
 public class Controller {
     public Button btnDeleteSongFromPlaylist;
     public Button btnNewSong;
     public Button btnAddSong;
     public Button btnEditSongs;
+    public Button btnPlay;
     @FXML
     private Button btnDeleteSong;
     @FXML
@@ -99,5 +101,14 @@ public class Controller {
             /* We have to adapt this method so that it actually deletes the Song.
             For now it is just closing everything. */
         }
+    }
+
+    public void PlayMusic(ActionEvent actionEvent) {
+        File file = new File("C:/Users/zkooh/IdeaProjects/MyTunes2.0/myTunes'songs/MyTunes/(Disc 2) 03 - Sunshine.mp3");
+        System.out.println("file:" + file.toURI().toString());
+        Media media = new Media(file.toURI().toString());
+        MediaPlayer mediaPlayer = new MediaPlayer(media);
+        mediaPlayer.play();
+
     }
 }
