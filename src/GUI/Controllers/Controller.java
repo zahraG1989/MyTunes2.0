95966cee7c05d74e49d72f3a74182c0d4f4bb30b
Chantal1208 <chan2292@easv365.dk>
2021-12-06 12:26:34 +0100

    Merge branch 'main' of /Users/chantalbrockmeyer/Documents/GitHub/MovieRecommendationSystem/MyTunes2.0 with conflicts.

--- src/GUI/Controllers/Controller.java
+++ src/GUI/Controllers/Controller.java
@@ -4,14 +4,37 @@ import javafx.embed.swing.JFXPanel;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 
+<<<<<<< Updated upstream
+=======
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+>>>>>>> Stashed changes
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
+import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
 
+import java.io.File;
+
+<<<<<<< Updated upstream
 public class Controller {
+=======
+import java.io.File;
+import java.io.IOException;
+
+public class Controller {
+    public Button btnDeleteSongFromPlaylist;
+    public Button btnNewSong;
+    public Button btnAddSong;
+    public Button btnEditSongs;
+    public Button btnPlay;
     @FXML
     private Button btnNewSong;
     @FXML
@@ -40,4 +63,15 @@ public class Controller {
 
     public void OpenNewPlaylist(MouseEvent mouseEvent) {
     }
+
+    public void PlayMusic(ActionEvent actionEvent) {
+        File file = new File("C:/Users/zkooh/IdeaProjects/MyTunes2.0/myTunes'songs/MyTunes/(Disc 2) 03 - Sunshine.mp3");
+        System.out.println("file:" + file.toURI().toString());
+        Media media = new Media(file.toURI().toString());
+        MediaPlayer mediaPlayer = new MediaPlayer(media);
+        mediaPlayer.play();
+
+    }
+
+
 }
