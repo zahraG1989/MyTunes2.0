eec9766a36dae5e1c90930811f51bc37e5d6e44e
Chantal1208 <chan2292@easv365.dk>
2021-12-13 14:12:50 +0100

    .

--- src/GUI/Controllers/playlistController.java
+++ src/GUI/Controllers/playlistController.java
@@ -1,16 +1,22 @@
 package GUI.Controllers;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.stage.Stage;
 
 public class playlistController {
-    public Button btnCancel;
-    public Button btnSavePlaylist;
+    @FXML
+    private Button btnCancel;
+    @FXML
+    private Button btnSavePlaylist;
 
     public void cancelEditingPlaylist(ActionEvent actionEvent) {
+
+        //if the user clicks on the "cancel" button in the PlaylistWindow, a window pops up that asks the user to confirm the cancellation.
+
         Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "Discard changes ?", ButtonType.YES, ButtonType.NO, ButtonType.CANCEL);
         alert.showAndWait();
 
