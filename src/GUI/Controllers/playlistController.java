6bd6a610a53612567d44a34c32f464131d147605
Chantal1208 <chan2292@easv365.dk>
2021-12-13 14:01:01 +0100

    cancel button Playlist window

--- src/GUI/Controllers/playlistController.java
+++ src/GUI/Controllers/playlistController.java
@@ -1,13 +1,24 @@
 package GUI.Controllers;
 
 import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.stage.Stage;
 
 public class playlistController {
     public Button btnCancel;
     public Button btnSavePlaylist;
 
     public void cancelEditingPlaylist(ActionEvent actionEvent) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "Discard changes ?", ButtonType.YES, ButtonType.NO, ButtonType.CANCEL);
+        alert.showAndWait();
+
+        if (alert.getResult() == ButtonType.YES) {
+
+            Stage stage = (Stage) btnCancel.getScene().getWindow();
+            stage.close();
+        }
     }
 
     public void savePlaylist(ActionEvent actionEvent) {
